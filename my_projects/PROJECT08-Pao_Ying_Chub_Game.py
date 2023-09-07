ece0d4f6d6a78cb4f8f1edc6987d3e050e61d582
tatathan <118846966+tatathan@users.noreply.github.com>
2023-09-07 07:57:32 +0700

    Create PROJECT08-Pao_Ying_Chub_Game.py

---
+++ my_projects/PROJECT08-Pao_Ying_Chub_Game.py
@@ -0,0 +1,27 @@
+def pizza():
+    from random import sample
+    print("Instruction: type 'exit' to exit the programe")
+    print("Start!")
+    options = ["hammer","scissor","paper"]
+    print(options)
+    point = 0
+
+    while True:
+        user_select = input("Choose one: ")
+        computer_select = sample(options, 1)[0]
+
+        if user_select == "exit":
+            print("Thx for playing")
+            break
+        elif (user_select == computer_select):
+            print("tie!")
+        elif (((user_select == "hammer") and (computer_select == "scissor")) or 
+          ((user_select == "scissor") and (computer_select == "paper")) or 
+          ((user_select == "paper") and (computer_select == "hammer"))):
+            print ("You Win!, +1point")
+            point += 1
+        else :
+            print ("You lose")
+        
+        print(f"Computer Select: {computer_select}")
+        print(f"You have {point} points.")
